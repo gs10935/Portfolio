663f0160c2b88db98afe5b1ace78125a269f4c6d
gs10935 <pgs10935@gmail.com>
2022-08-08 19:22:01 +0900

    Portfolio

---
+++ PortfolioScript/EnemySight.cs
@@ -0,0 +1,295 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BodySearch))]
+public class EnemySight : MonoBehaviour
+{
+    // 이 스크립트에선 적의 시야와 어그로시트 관리에 대해 다룹니다.
+    // 적의 시야각과 시야거리 등을 설정할 수 있으며 범위 안의 타겟을 탐색하여 어그로시트에 추가합니다.
+    // 적은 타겟의 마지막 위치를 기억할 수 있습니다. 타겟과의 거리가 어느정도 떨어진 상태에서 장애물 등에 가려진다면 해당위치까지 추적하고, 포착에 실패하면 타겟을 로스트합니다.
+
+    #region 변수
+    [SerializeField] private float _viewAnglel, _viewDistance, _viewHeight, _traceDistance, _confirmDistance, _attackAngle; //탐색 옵션
+    [SerializeField] private LayerMask _targerMask, _obstacleMask; //타겟 마스크, 장애물
+    [SerializeField] private string[] _targetTag; //타겟 태그
+
+    public List<TargetList> currentTargetList = new List<TargetList>(); //현재 타겟 목록
+
+    private Vector3 _lastTargetPos; //마지막 태겟 위치
+    private bool _keepTarget; //타겟 주시상태
+    private float _sightInterval; //업데이트 주기
+
+    private Transform _charbody; //캐릭터 트랜스폼
+    private CharacterState _charstate; //캐릭터 상태
+    private bool _drawRay = true;
+
+    private void Start()
+    {
+        _charbody = GetComponent<BodySearch>().CharacterBody.transform;
+        _charstate = GetComponent<BodySearch>().CharacterBody.GetComponent<CharacterManager>()._Controller.GetComponent<CharacterState>();
+    }
+    #endregion
+
+    private void Update()
+    {
+        if (_sightInterval > 0)
+            _sightInterval -= Time.deltaTime;
+        else
+        {
+            _sightInterval = 0.4f;
+            AggroManage();
+        }
+    }
+
+    #region 시야에서 타겟 서치
+    private void FindVisibleTargets()
+    {
+        Vector3 facingDir = new Vector3(_charbody.localScale.x, 0, 0); //횡스크롤 시야
+        Vector3 _pos = _charbody.position + Vector3.up * _viewHeight; //시야 시작점
+        DrawView(_viewDistance, _viewAnglel, _traceDistance, _pos); //시야각 그리기
+
+        #region 새로운 타겟을 탐색
+        //새로운 타겟을 탐색
+        if (!_charstate.charstate_target.Target._target)
+        {
+            //시야 거리 내 타겟레이어의 모든 컬라이더 받아오기
+            Collider[] targets = Physics.OverlapSphere(_pos, _viewDistance, _targerMask);
+
+            for (int i = 0; i < targets.Length; i++)
+                for (int s = 0; s < _targetTag.Length; s++)
+                    if (targets[i].tag.Equals(_targetTag[s]))
+                    {
+                        Transform target = targets[i].transform;
+                        Vector3 targetposition = target.transform.position; //타겟 위치
+                        Vector3 dirToTarget = (targetposition - _pos).normalized; //타겟의 방향
+
+                        //_transform.forward와 dirToTarget은 모두 단위벡터이므로 내적값은 두 벡터가 이루는 각의 Cos값과 같다.
+                        //내적값이 시야각/2의 Cos값보다 크면 시야에 들어온 것이다.
+                        if (Vector3.Dot(facingDir, dirToTarget) > Mathf.Cos((_viewAnglel / 2) * Mathf.Deg2Rad))
+                        {
+                            float distToTarget = Vector3.Distance(_pos, targetposition);
+                            if (!Physics.Raycast(_pos, dirToTarget, distToTarget, _obstacleMask)) //장애물이 없으면
+                            {
+                                Debug.DrawLine(_pos, targetposition, Color.red);
+                                AddAggroTarget(target, 0); //어그로 시트에 타겟 추가
+                            }
+                        }
+
+                        break;
+                    }
+        }
+        #endregion
+        #region 기존 타겟을 추적
+        //기존 타겟을 추적
+        else if (_charstate.charstate_target.Target._target)
+        {
+            //추적 거리에서 타겟을 검색
+            Collider[] tracetargets = Physics.OverlapSphere(_pos, _traceDistance, _targerMask); 
+            bool isWatchTarget = false; //주시상태 체크
+            bool isLost = true; //타겟 로스트 유무
+
+            for (int i = 0; i < tracetargets.Length; i++)
+                if (System.Object.ReferenceEquals(_charstate.charstate_target.Target._target, tracetargets[i].transform))
+                {
+                    isWatchTarget = true;
+                    Transform target = tracetargets[i].transform;
+                    Vector3 targetposition = target.transform.position; //타겟 위치
+                    Vector3 dirToTarget = (targetposition - _pos).normalized; //타겟의 방향
+                    float distToTarget = Vector3.Distance(_pos, targetposition); //타겟과의 거리
+
+                    //▶ 타겟 추적
+                    if (!Physics.Raycast(_pos, dirToTarget, distToTarget, _obstacleMask) || _keepTarget) //사이에 장애물이 없거나, 주시상태라면
+                    {
+                        Debug.DrawLine(_pos, targetposition, Color.red);
+                        _lastTargetPos = targetposition; //타겟 위치 기억
+                        isWatchTarget = true; //타겟 주시 활성화
+                        isLost = false; //만약 주시상태라면 타겟을 계속 포착한다
+                    }
+
+                    break;
+                }
+
+            _keepTarget = isWatchTarget; //주시상태 갱신
+
+            if (!isWatchTarget && Vector3.Distance(_pos, _lastTargetPos) < _confirmDistance) //최종 기억위치에서 타겟 포착을 실패했다면 타겟을 로스트한다
+                DelAggroTarget();
+        }
+        #endregion
+    }
+
+
+    #endregion
+
+    #region DrawRay
+    private void DrawView(float viewdistance, float angle, float tracedistance, Vector3 Pos)
+    {
+        //디버그용 주시각 그리기
+        if (Input.GetKeyDown(KeyCode.P))
+            _drawRay = !_drawRay;
+
+        if (_drawRay)
+        {
+            float Distance;
+
+            if (_charstate.charstate_target.Target._target)
+                Distance = tracedistance;
+            else
+                Distance = viewdistance;
+
+            Vector3 leftBoundary = DirFromAngle(-angle / 2);
+            Vector3 rightBoundary = DirFromAngle(angle / 2);
+            Debug.DrawLine(Pos, Pos + leftBoundary * Distance, Color.blue);
+            Debug.DrawLine(Pos, Pos + rightBoundary * Distance, Color.blue);
+
+            if (_charstate.charstate_target.Target._target)
+                Debug.DrawLine(_lastTargetPos, _lastTargetPos + Vector3.up, Color.red);
+            if (_keepTarget && _charstate.charstate_target.Target._target)
+                Debug.DrawLine(Pos, _charstate.charstate_target.Target._target.transform.position, Color.red);
+        }
+    }
+
+    //캐릭터 방향에 따른 시야각 반환
+    private Vector3 DirFromAngle(float angleInDegrees)
+    {
+        //좌우 회전값 갱신
+        if (_charstate.charstate_behavior.FacingRight) //왼쪽방향
+            angleInDegrees += _charbody.eulerAngles.y + 90f;
+        else
+            angleInDegrees += _charbody.eulerAngles.y - 90f;
+
+        //경계 벡터값 반환
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+    #endregion
+
+    #region 어그로 리스트 관리
+    public void AggroManage() // 어그로 시트 관리
+    {
+        //타겟지정이 불가능한 대상은 지운다
+        if (_charstate.charstate_target.Target._target && !CheckTarget(_charstate.charstate_target.Target._target))
+            DelAggroTarget();
+
+        //다음 어그로 타겟 잡기
+        if (_charstate.Check_Action(true, false, false, true, false, true))
+            NextAggroTarget(true);
+    }
+
+    private bool CheckTarget(Transform _target)
+    {
+        bool _check = true;
+        CharacterState _targetstate = _target.GetComponent<CharacterManager>()._Controller.GetComponent<CharacterState>();
+
+        if(!_target.gameObject.activeInHierarchy || _targetstate.charstate_limit.State_Death) //타겟이 비활성화 되거나 죽었다면
+            _check = false;
+
+        if(_check)
+            for (int i = 0; i < _targetTag.Length; i++)
+                if(_targetTag[i].Equals(_target.tag)) //타겟 태그를 체크한다
+                {
+                    _check = true;
+                    break;
+                }
+                else
+                {
+                    _check = false;
+                }
+
+        return _check;
+    }
+
+    public void AddAggroTarget(Transform _target, float _aggro)
+    {
+        if (_target.GetComponent<CharacterManager>() && CheckTarget(_target) && _charstate)
+        {
+            bool isExsit = false;
+
+            //이미 어그로 시트에 등록 된 타겟인지 체크한다
+            for (int i = currentTargetList.Count - 1; i >= 0; i--)
+                if (currentTargetList[i]._target)
+                {
+                    if (System.Object.ReferenceEquals(currentTargetList[i]._target, _target))
+                    {
+                        currentTargetList[i]._aggro += _aggro;
+                        if (_charstate.charstate_target.Target._aggro < currentTargetList[i]._aggro) //현재 타겟보다 어그로 수치가 높다면 갱신
+                        {
+                            _charstate.charstate_target.Target._target = currentTargetList[i]._target;
+                            _charstate.charstate_target.Target._aggro = currentTargetList[i]._aggro;
+                        }
+
+                        isExsit = true;
+                        break;
+                    }
+                }
+                else
+                    currentTargetList.RemoveAt(i);
+
+            //기존 어그로 시트에 없다면
+            if (!isExsit)
+            {
+                TargetList newList = new TargetList();
+                newList._target = _target;
+                newList._aggro = _aggro;
+                currentTargetList.Add(newList);
+            }
+        }
+    }
+
+    private void DelAggroTarget() //현재타겟을 삭제
+    {
+        if (_charstate.Check_Action(true, false, false, true, false, true))
+        {
+            for (int i = 0; i < currentTargetList.Count; i++)
+                if (System.Object.ReferenceEquals(currentTargetList[i]._target, _charstate.charstate_target.Target._target))
+                {
+                    currentTargetList.RemoveAt(i);
+                    _charstate.charstate_target.Target._target = null;
+                    _charstate.charstate_target.Target._aggro = 0;
+                    break;
+                }
+
+            NextAggroTarget();
+        }
+    }
+    
+    private void NextAggroTarget() //다음 타겟 잡기
+    {
+        //어그로 시트에서 어그로 수치가 가장 높은 타겟으로 설정한다.
+        float _dist = 0;
+        for (int i = currentTargetList.Count - 1; i >= 0; i--)
+            if (currentTargetList[i]._target)
+            {
+                //어그로 자연감소
+                if (_isManage)
+                {
+                    if (currentTargetList[i]._aggro > 1000)
+                        currentTargetList[i]._aggro = 1000;
+                    if (currentTargetList[i]._aggro > 0)
+                        currentTargetList[i]._aggro -= 40f * Time.deltaTime;
+                    else
+                        currentTargetList[i]._aggro = 0;
+
+                    if (_charstate.charstate_target.Target._target && System.Object.ReferenceEquals(_charstate.charstate_target.Target._target, currentTargetList[i]._target))
+                        _charstate.charstate_target.Target._aggro = currentTargetList[i]._aggro;
+                }
+
+                //타겟 교체
+                if (!_charstate.charstate_target.Target._target)
+                {
+                    _dist = Vector3.Distance(_charbody.position, currentTargetList[i]._target.position);
+                    _charstate.charstate_target.Target._target = currentTargetList[i]._target;
+                    _charstate.charstate_target.Target._aggro = currentTargetList[i]._aggro;
+                }
+                else if (_charstate.charstate_target.Target._aggro < currentTargetList[i]._aggro
+                    || (_charstate.charstate_target.Target._aggro <= currentTargetList[i]._aggro && Vector3.Distance(_charbody.position, currentTargetList[i]._target.position) < _dist))
+                {
+                    _dist = Vector3.Distance(_charbody.position, currentTargetList[i]._target.position);
+                    _charstate.charstate_target.Target._target = currentTargetList[i]._target.transform;
+                    _charstate.charstate_target.Target._aggro = currentTargetList[i]._aggro;
+                }
+            }
+            else
+                currentTargetList.RemoveAt(i);
+    }
+    #endregion
+}
---
+++ PortfolioScript/RangeAction.cs
@@ -0,0 +1,160 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+[RequireComponent(typeof(BodySearch))]
+public class RangeAction : MonoBehaviourPunCallbacks
+{
+    // 이 스트립트에선 총기를 사용한 액션과 애니메이션을 다룹니다.
+    // 총기마다 조준, 발사, 장전 등의 애니메이션 타입이 다르며 작성된 애니메이션을 입력된 값으로 사용할 수 있도록 합니다.
+    // 또한, 조준 전과 후의 일부 애니메이션의 진행도를 이어받을 수 있도록 하여 2D 스프라이트 애니메이션으로도 자연스럽게 모션이 전환됩니다.
+
+    #region 변수 & 초기화
+    public LayerMask TargetMask; //타겟의 레이어마스크
+    public float ViewDistance; //조준 거리
+    public bool TargetAutoAiming, ShotCancel; //자동조준옵션, 조준취소가능상태
+
+    private Transform CharacterBody; //플레이어블 캐릭터
+    private CharacterState charstate; //플레이어의 행동상태
+    private Status status; //플레이어의 스테이터스
+    private CharacterAnimation charanim; //플레이어의 애니메이션 제어
+    private CharacterControl charctrl; //플레이어의 캐릭터컨트롤러
+    private Animator mainanimator, weaponanimator; //플레이어의 메인 애니메이터, 무기 애니메이터
+
+    void Start()
+    {
+        CharacterBody = GetComponent<BodySearch>().CharacterBody.transform;
+        charstate = GetComponent<BodySearch>().CharacterBody.GetComponent<CharacterManager>()._Controller.GetComponent<CharacterState>();
+        status = GetComponent<BodySearch>().CharacterBody.GetComponent<CharacterManager>()._Controller.GetComponent<Status>();
+        charctrl = GetComponent<BodySearch>().CharacterBody.GetComponent<CharacterManager>()._Controller.GetComponent<CharacterControl>();
+        charanim = GetComponent<BodySearch>().CharacterBody.GetComponent<CharacterManager>()._Sprite.GetComponent<CharacterAnimation>();
+        mainanimator = GetComponent<BodySearch>().CharacterBody.GetComponent<Animator>();
+        weaponanimator = GetComponent<BodySearch>().CharacterBody.GetComponent<CharacterManager>().WeaponSprite.GetComponent<Animator>();
+    }
+    #endregion
+
+    void Update()
+    {
+        InputAction();
+    }
+
+    // 인풋 액션
+    private void InputAction()
+    {
+        // 조준 가능한 상태일때 지정된 조준키를 입력하여 조준상태 액션으로 전환합니다. 조준상태에서 추가로 키를 입력하여 총기액션을 사용할 수 있습니다.
+        if (0 <= charstate.charstate_action.Sprite_MotionChange && charstate.charstate_action.Sprite_MotionChange < 1000
+            && Input.GetButton("keyV") && !status.status_equip.SubWeapon.Name.Equals("") 
+            && (charstate.Check_Action(true, false, false, false, false, true) || CheckExceptions()))
+        {
+            // 무기의 고유 애니메이션값을 받아 총기액션을 구현
+            WeaponAction(status.status_equip.SubWeapon.motion_startAim, status.status_equip.SubWeapon.motion_Aim, status.status_equip.SubWeapon.motion_shot, status.status_equip.SubWeapon.motion_Reload);
+        }
+        else if(0 < charstate.charstate_action.Sprite_MotionChange && charstate.charstate_action.Sprite_MotionChange < 1000 && ShotCancel)
+        {
+            if(charstate.charstate_action.ActionNum == 1)
+                charanim.GetAnimationPlaytime("Walk", 0, mainanimator, 1); //걷기 애니메이션에서 진행도를 이어받는다
+            charstate.charstate_action.Sprite_MotionChange = 0;
+            charstate.charstate_target.Target._target = null;
+            mainanimator.SetFloat("MotionSpeed", 1);
+            ShotCancel = false;
+        }
+    }
+
+    // 총기 조작
+    private void WeaponAction(int motion_StartAim, int motion_Aim, int motion_Shot, int motion_Reload)
+    {
+        //자동조준 옵션 적용
+        if(!CheckExceptions() && TargetAutoAiming)
+            TargetSearch();
+        else
+            mainanimator.SetFloat("MotionSpeed", 1);
+
+        //첫 모션 전환시점이고 본체 애니메이션이 걷기 또는 뛰기 중이라면, 애니메이션 진행도를 이어받는다
+        if (charstate.charstate_action.Sprite_MotionChange == 0 && 
+            (charstate.charstate_action.ActionNum == 1 || charstate.charstate_action.ActionNum == 2))
+            charanim.GetAnimationPlaytime("AimWalk", 1, mainanimator);
+
+        //무기 꺼내기 애니메이션
+        if (charstate.charstate_action.Sprite_MotionChange <= 0 && charstate.charstate_action.Sprite_MotionChange != motion_StartAim)
+        {
+            ShotCancel = false;
+            charstate.charstate_action.Sprite_MotionChange = motion_StartAim;
+        }
+
+        //무기 조준 애니메이션
+        if (charstate.charstate_action.Sprite_MotionChange != motion_Shot && charstate.charstate_action.Sprite_MotionChange != motion_Reload && ShotCancel)
+            charstate.charstate_action.Sprite_MotionChange = motion_Aim; 
+
+        //무기 사용 애니메이션
+        if (Input.GetButton("keyZ") && (charstate.charstate_action.Sprite_MotionChange == motion_Aim || ShotCancel) 
+            && status.status_equip.SubWeapon.MaxMagazine > 0 && status.status_equip.SubWeapon.Magazine >= status.status_equip.SubWeapon.RequireBullet && status.status_base.Sta >= status.status_equip.SubWeapon.RequireSta)
+        {
+            if (charstate.charstate_action.Sprite_MotionChange == motion_Shot)
+                charanim.ResetAnimRate(new string[] { "Weapon" });
+            ShotCancel = false;
+            charstate.charstate_action.Sprite_MotionChange = motion_Shot;
+            status.status_equip.SubWeapon.Magazine -= status.status_equip.SubWeapon.RequireBullet;
+            status.status_base.Sta -= status.status_equip.SubWeapon.RequireSta;
+        }
+        //무기 장전 애니메이션
+        else if (((Input.GetButton("keyS") && status.status_equip.SubWeapon.Magazine < status.status_equip.SubWeapon.MaxMagazine)
+            || (Input.GetButton("keyZ") && status.status_equip.SubWeapon.Magazine <= 0 && status.status_equip.SubWeapon.TotalMagazine > 0))
+            && charstate.charstate_action.Sprite_MotionChange != motion_Reload && status.status_equip.SubWeapon.TotalMagazine > 0 && ShotCancel
+            && !CheckExceptions())
+        {
+            ShotCancel = false;
+            charstate.charstate_action.Sprite_MotionChange = motion_Reload;
+        }
+
+        //타겟 자동 지정 옵션 활성화
+        if (Input.GetButtonDown("keyA"))
+            TargetAutoAiming = !TargetAutoAiming;
+    }
+
+    //자동조준 옵션 사용시, 타겟 서치
+    private void TargetSearch()
+    {
+        //가장 가까운 적들 중 z축이 가까운 적을 찾는다.
+        Collider[] targets = Physics.OverlapBox(CharacterBody.transform.position, new Vector3(ViewDistance, 2, 0.8f), Quaternion.identity, TargetMask);
+        Transform Target = null;
+        for (int i = 0; i < targets.Length; i++)
+        {
+            if (targets[i].tag.Equals("Enemy") && !targets[i].GetComponent<CharacterManager>().isDeath)
+            {
+                if (!Target)
+                    Target = targets[i].transform;
+                else if (Target && Vector3.Distance(CharacterBody.transform.position, Target.position) > Vector3.Distance(CharacterBody.transform.position, targets[i].transform.position)
+                    && Mathf.Abs(CharacterBody.transform.position.z - targets[i].transform.position.z) < 0.75f)
+                    Target = targets[i].transform;
+            }
+        }
+
+        charstate.charstate_target.Target._target = Target;
+
+        //서치중 이동 처리
+        if (charstate.charstate_target.Target._target && TargetAutoAiming)
+        {
+            charctrl.ForceDirection(charstate.charstate_target.Target._target.transform.position);
+            if ((charstate.charstate_behavior.FacingRight && Input.GetAxis("Horizontal") > 0) || (!charstate.charstate_behavior.FacingRight && Input.GetAxis("Horizontal") < 0))
+                mainanimator.SetFloat("MotionSpeed", 1);
+            else if ((charstate.charstate_behavior.FacingRight && Input.GetAxis("Horizontal") < 0) || (!charstate.charstate_behavior.FacingRight && Input.GetAxis("Horizontal") > 0))
+                mainanimator.SetFloat("MotionSpeed", -1);
+        }
+        else
+            mainanimator.SetFloat("MotionSpeed", 1);
+    }
+
+     //특수 행동상태에서 조준 가능여부
+    private bool CheckExceptions()
+    {
+        bool check = false;
+        if(charstate.charstate_action.ActionNum > 301 && charstate.charstate_action.ActionNum < 310) //로프에 매달린 상태로 조준
+        {
+            check = true;
+        }
+
+        return check;
+    }
+}
